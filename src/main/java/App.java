b19ce5d672f17173a5d9c471e5f8f05642c0a812
Ilyaladnyuk <finra@mail.ru>
2021-11-25 21:09:15 +0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+public class App {
+    private int[] arr;
+
+    public static void main(String[] args) {
+        App app = new App(new int[]{5, 3, 1, 0, 8, 6, 7, 8});
+        app.changeOfLocation(1, 2);
+        System.out.println(app);
+        func1(app.arr);
+
+        Box<Orange> orangeBox = new Box<>();
+        Box<Apple> appleBox = new Box<>();
+        appleBox.addFruit(new Apple(), 15);
+        orangeBox.addFruit(new Orange(),15);
+        System.out.println();
+        System.out.println("Коробка с яблоками весит: " + appleBox.getWeight());
+        System.out.println("Коробка с апельсинами весит: " + orangeBox.getWeight());
+        System.out.println(appleBox.compare(orangeBox));
+        Box<Apple> appleBox1 = new Box<>();
+        appleBox.pourTo(appleBox1);
+
+    }
+
+    public App(int[] arr) {
+        this.arr = arr;
+    }
+
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("App{");
+        sb.append("arr=").append(Arrays.toString(arr));
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public void changeOfLocation(int index1, int index2) {
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+    public static void func1(int arr[]){
+        ArrayList<Integer> array_list = new ArrayList<>();
+            for (int i = 0; i < arr.length; i++)
+            array_list.add(new Integer(arr[i]));
+            System.out.print(array_list);
+    }
+}
---
+++ src/main/java/Box.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class Box<T extends Fruit>  {
+
+    private List<T> box;
+
+    public Box() {
+        this.box = new ArrayList<>();
+    }
+
+    public void addFruit(T fruit, int amount){
+        for(int i = 0; i <amount; i++){
+            box.add(fruit);
+        }
+    }
+
+    public float getWeight(){
+        float weight = 0.0f;
+        for(T o : box){
+            weight += o.getWeight();
+        }return weight;
+    }
+
+    public boolean compare(Box anotherBox) {
+        if(getWeight() == anotherBox.getWeight()){
+            System.out.println("Коробки весят одинаково");
+            return true;
+        }
+        System.out.println("Коробки имею ют разный вес");
+        return false;
+    }
+
+    public void pourTo(Box <T>anotherBox){
+        anotherBox.box.addAll(box);
+        box.clear();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Box{");
+        sb.append("box=").append(box);
+        sb.append('}');
+        return sb.toString();
+    }
+//    public float getWeight() {
+//        float sum = 0;
+//        for (int i = 0; i < fruits.size(); i++) {
+//            sum += (i);
+//        }
+//        System.out.println("Вес коробки: " + sum);
+//        return sum;
+//    }
+
+}
